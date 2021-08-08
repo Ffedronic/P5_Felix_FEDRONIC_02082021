d58133e3afd97299ca6b62fdaf4f9f76674d8d35
Felix Fedronic <fedronicfelix@gmail.com>
2021-08-08 23:11:08 +0200

    page de vue liste article terminée

---
+++ front-end/index.js
@@ -0,0 +1,24 @@
+fetch("http://localhost:3000/api/teddies/")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(listArticle) {
+    console.log(listArticle);
+    document.getElementById('article-length').innerHTML += `(${listArticle.length} articles)` ; 
+    for(let article of listArticle) {
+        document.getElementById('articles-group').innerHTML += `<article class="card" id="${article._id}" style="width: 20em;">
+                                                                    <img src="${article.imageUrl}" alt="${article.name} ours en peluce fait main">
+                                                                    <div class="card-body">
+                                                                        <h3 class="card-title">${article.name}</h3>
+                                                                        <p class="card-text">${article.description}</p>
+                                                                        <a href="#" class="btn btn-primary">Détails</a>
+                                                                    </div>
+                                                                </article>`
+    };
+    document.getElementById('color')
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
