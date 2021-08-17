2cdccc62a457ec89fd052e722700be07692dc2fa
Felix Fedronic <fedronicfelix@gmail.com>
2021-08-17 13:59:40 +0200

    cart.js récupération des objets dans le local storage en cours

--- front-end/view/cart/cart.js
+++ front-end/view/cart/cart.js
@@ -1,3 +1,27 @@
 //déclaration de la variable products qui contiendra les clés productReadyToBuy 
-let products = JSON.parse(localStorage.getItem("produit")) ;
-console.log(products) ;
+let listOfProductStorage = JSON.parse(localStorage.getItem("produit")) ;
+
+//affichage du nombre d'articles dans le localStorage sur la page du panier
+let lengthOfProductsInCart = document.getElementById('cardProductsLength') ;
+lengthOfProductsInCart.innerHTML += `Il contient ${listOfProductStorage.length} articles.` ;
+
+
+//récupération des id des produits dans la base de données
+fetch("http://localhost:3000/api/teddies/")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(listArticle) {
+      const listOfId = [] ;
+      for(let article of listArticle) {
+        listOfId.push(article._id) ;
+      } ;
+      //filtrer la liste des articles dans le localStorage
+      for(let id of listOfId) {
+        const idInLocalStorage = listOfProductStorage.filter(listOfProductStorage => listOfProductStorage.id == id ) ;
+        console.log(idInLocalStorage) ;
+      };
+  })
+
